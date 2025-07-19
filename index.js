7df5b267609a6208efad8803551f3435cad05be5
Riley Pruitt <45256761-ripr1656@users.noreply.replit.com>
2025-07-19 17:28:58 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+require("dotenv").config();
+
+const { Client, GatewayIntentBits } = require("discord.js");
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+});
+
+client.once("ready", () => {
+  console.log(`✅ Logged in as ${client.user.tag}`);
+});
+
+client.on("messageCreate", (message) => {
+  if (message.author.bot) return;
+});
+
+client.login(process.env.DISCORD_TOKEN);
